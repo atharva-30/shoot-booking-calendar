d5f178a3a1880c084989b6607d1838084302e762
atharva-30 <113660975+atharva-30@users.noreply.github.com>
2026-07-17 03:48:16 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { Shoot, AppStats, ViewMode } from './types';
-import { exportToCSV, getMockShoots } from './utils';
+import { getMockShoots, exportToCSV } from './utils';
 import { 
   collection, 
   onSnapshot, 
